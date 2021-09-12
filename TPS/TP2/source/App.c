a18540e1d54b9a2c029a45dd9cc77ee516d2cb2e
Matias Bergerman <matias.bergerman@hotmail.com>
2021-09-11 21:15:37 -0300

    Update

--- TPS/TP2/source/App.c
+++ TPS/TP2/source/App.c
@@ -42,6 +42,12 @@ typedef enum{
 }menuEvent_t;
 
 typedef enum{
+	EVENT_EDITOR_PREV,
+	EVENT_EDITOR_NEXT,
+	EVENT_EDITOR_NONE
+}editorEvent_t;
+
+typedef enum{
 	STATE_MAIN,
 	STATE_ID,
 	STATE_PIN,
@@ -70,19 +76,32 @@ static const char* menu_item_strings[] = {"Enter ID", "Edit", "Brightness", "Spe
 
 static void blink_callback();
 
-static void state_main(menuEvent_t);
+static menuState_t state_main(menuEvent_t);
+
+static void start_number_editor(uint8_t editor_len, bool flag_next, bool flag_hidden);
+
+//static void number_editor_left();
+
+//static void number_editor_right();
+
+
+// Si clickeas en '<' devuelve PREV
+// Si clickeas en '>' devuelve NEXT
+// Si clickeas en un dígito devuelve NONE
+
+//static editorEvent_t number_editor_click();
 
 /*******************************************************************************
  * GLOBAL VARIABLES ?)
  ******************************************************************************/
 
-menuState_t menu_state = STATE_MAIN;
 menuItem_t menu_item = ITEM_ID;
 bool edit_flag = false;
 
 uint8_t number_editor_len = 0;
 uint8_t number_editor_array[EDITOR_BUFFER_LEN];
-bool number_editor_flag_ok = true;
+bool number_editor_flag_next = true;
+bool number_editor_flag_hidden = false;
 
 tim_id_t encoder_tim_id;
 uint32_t blink_period = 500;
@@ -112,19 +131,36 @@ void App_Init (void)
 /* Función que se llama constantemente en un ciclo infinito */
 void App_Run (void)
 {
+	static menuState_t menu_state = STATE_MAIN;
 	menuEvent_t event = EVENT_NONE;
 
 	//if(readerGetStatus()){
 	//	event = EVENT_CARD;
 	/*}else*/
 	if(encoderGetStatus()){
-		event = encoderGetEvent();
+		event = encoderGetEvent();	//Ya que en el enum se definen los mismos índices
 	}
 
 	if(event != EVENT_NONE){
 		switch(menu_state){
 		case STATE_MAIN:
-			state_main(event); break;
+			menu_state = state_main(event);
+			break;
+		case STATE_ID:
+			//menu_state = state_id(event);
+			break;
+		case STATE_PIN:
+			//menu_state = state_pin(event);
+			break;
+		case STATE_ADMIN:
+			//menu_state = state_admin(event);
+			break;
+		case STATE_BRIGHTNESS:
+			//menu_state = state_brightness(event);
+			break;
+		case STATE_SPEED:
+			//menu_state = state_speed(event);
+			break;
 		default: break;
 		}
 	}
@@ -142,25 +178,42 @@ static void blink_callback(){
    	timerStart(encoder_tim_id, TIMER_MS2TICKS(blink_period), TIM_MODE_SINGLESHOT, blink_callback);
 }
 
-static void start_number_editor(uint8_t editor_len, bool flag_ok){
+// editor_len: cuántos numero queres editar?
+// flag_next: si queres que haya un botón de 'ok' (flechita para la derecha)
+static void start_number_editor(uint8_t editor_len, bool flag_next, bool flag_hidden){
 	number_editor_len = editor_len;
+	number_editor_flag_hidden = flag_hidden;
 
 	dispStopAutoscroll();
 	dispSetBufferPos(0);
 	dispClearBuffer();
-	dispWriteChar(0, CHAR_PREV);
+
+	dispWriteChar(0, CHAR_PREV);	// '<'
 	for(uint8_t i = 0; i < editor_len; i++){
 		number_editor_array[i] = 0;
-		dispWriteChar(0, '0');
+		dispWriteChar(i+1, (flag_hidden)? '-': '0');
 	}
-	number_editor_flag_ok = flag_ok;
+	number_editor_flag_next = flag_next;
 
-	if(flag_ok){
-		dispWriteChar(editor_len + 1, CHAR_NEXT);
+	if(flag_next){
+		dispWriteChar(editor_len + 1, CHAR_NEXT);  // '>'
 	}
 }
 
-static void state_main(menuEvent_t event){
+//static void number_editor_left();
+
+//static void number_editor_right();
+
+
+// Si clickeas en '<' devuelve PREV
+// Si clickeas en '>' devuelve NEXT
+// Si clickeas en un dígito devuelve NONE
+
+//static editorEvent_t number_editor_click();
+
+static menuState_t state_main(menuEvent_t event){
+	menuState_t next_state = STATE_MAIN;
+
 	switch(event){
 	case EVENT_ENC_LEFT:
 		menu_item = (menu_item + NUM_ITEMS - 1) % NUM_ITEMS;
@@ -174,26 +227,28 @@ static void state_main(menuEvent_t event){
 		switch(menu_item){
 		case ITEM_ID:
 			edit_flag = false;
-			menu_state = STATE_ID;
-			start_number_editor(ID_LENGTH, true);
+			next_state = STATE_ID;
+			start_number_editor(ID_LENGTH, true, false);
 			break;
 		case ITEM_EDIT:
 			edit_flag = true;
-			menu_state = STATE_ID;
-			start_number_editor(ID_LENGTH, true);
+			next_state = STATE_ID;
+			start_number_editor(ID_LENGTH, true, false);
 			break;
 		case ITEM_BRIGHTNESS:
-			menu_state = STATE_BRIGHTNESS;
+			next_state = STATE_BRIGHTNESS;
 			start_number_editor(1, false);
 			break;
 		case ITEM_SPEED:
-			menu_state = STATE_SPEED;
+			next_state = STATE_SPEED;
 			start_number_editor(1, false);
 			break;
 		default: break;
 		}
 	default: break;
 	}
+
+	return next_state;
 }
 
 /*******************************************************************************
