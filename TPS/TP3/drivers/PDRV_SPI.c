eb09cc48ccd7d7038b9ce952eaa1e0bf10d27d16
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2021-10-16 15:14:19 -0300

    SPI driver main functions compete (falta testear crack)

--- TPS/TP3/drivers/PDRV_SPI.c
+++ TPS/TP3/drivers/PDRV_SPI.c
@@ -50,7 +50,7 @@
 #define SPI0_PCS4_ALT    2
 #define SPI0_PCS5_ALT    3
 
-// SPI1 PORT, PINOUT, AND MUX ALT
+// SPI1 PORT, PINOUT, AND MUX ALT (NOT ACCESSIBLE IN FRDM-K64F DEVELOPMENT BOARD)
 #define SPI1_SCLK_PORT   4
 #define SPI1_SOUT_PORT   4
 #define SPI1_SIN_PORT    4
@@ -78,7 +78,7 @@
 #define SPI1_PCS2_ALT    2
 #define SPI1_PCS3_ALT    2
 
-// SPI2 PORT, PINOUT, AND MUX ALT
+// SPI2 PORT, PINOUT, AND MUX ALT (NOT ACCESSIBLE IN FRDM-K64F DEVELOPMENT BOARD)
 #define SPI2_SCLK_PORT   1
 #define SPI2_SOUT_PORT   1
 #define SPI2_SIN_PORT    1
@@ -188,10 +188,10 @@ static SPI_mode_t port_mode[3];
  *******************************************************************************
  ******************************************************************************/
 
-void SPI_init(uint8_t spi_port, SPI_mode_t mode, uint8_t chip_selects, bool PCSIS)
+void SPI_Init(uint8_t spi_port, SPI_config_t config)
 {
-    port_mode[spi_port] = mode; //remember port mode
-    chip_selects = ((chip_selects == 0)? 1 : chip_selects); //Minimum number of peripherals is 1
+    port_mode[spi_port] = config.mode; //remember port mode
+    config.chip_selects = ((chip_selects == 0)? 1 : config.chip_selects); //Minimum number of peripherals is 1
     
     //Clock Gating
     switch(spi_port)
@@ -221,32 +221,32 @@ void SPI_init(uint8_t spi_port, SPI_mode_t mode, uint8_t chip_selects, bool PCSI
     PORT_PTRS[SPI_SIN_PORT(spi_port)]->PCR[SPI_SIN_PIN(spi_port)] |= PORT_PCR_MUX(SPI_SIN_ALT(spi_port));
     
     // Initialize peripheral chip selects only in master mode
-    if(mode)
+    if(config.mode)
     {
         PORT_PTRS[SPI_PCS0_PORT(spi_port)]->PCR[SPI_PCS0_PIN(spi_port)] = 0x0; //Clear
         PORT_PTRS[SPI_PCS0_PORT(spi_port)]->PCR[SPI_PCS0_PIN(spi_port)] |= PORT_PCR_MUX(SPI_PCS0_ALT(spi_port));
 
-        if (chip_selects > 1 && spi_port != SPI2)
+        if (config.chip_selects > 1 && spi_port != SPI2)
         {
             PORT_PTRS[SPI_PCS1_PORT(spi_port)]->PCR[SPI_PCS1_PIN(spi_port)] = 0x0; //Clear
             PORT_PTRS[SPI_PCS1_PORT(spi_port)]->PCR[SPI_PCS1_PIN(spi_port)] |= PORT_PCR_MUX(SPI_PCS1_ALT(spi_port));
         }
-        if (chip_selects > 2 && spi_port != SPI2)
+        if (config.chip_selects > 2 && spi_port != SPI2)
         {
             PORT_PTRS[SPI_PCS2_PORT(spi_port)]->PCR[SPI_PCS2_PIN(spi_port)] = 0x0; //Clear
             PORT_PTRS[SPI_PCS2_PORT(spi_port)]->PCR[SPI_PCS2_PIN(spi_port)] |= PORT_PCR_MUX(SPI_PCS2_ALT(spi_port));
         }
-        if (chip_selects > 3 && spi_port != SPI2)
+        if (config.chip_selects > 3 && spi_port != SPI2)
         {
             PORT_PTRS[SPI_PCS3_PORT(spi_port)]->PCR[SPI_PCS3_PIN(spi_port)] = 0x0; //Clear
             PORT_PTRS[SPI_PCS3_PORT(spi_port)]->PCR[SPI_PCS3_PIN(spi_port)] |= PORT_PCR_MUX(SPI_PCS3_ALT(spi_port));
         }
-        if (chip_selects > 4 && spi_port == SPI0)
+        if (config.chip_selects > 4 && spi_port == SPI0)
         {
             PORT_PTRS[SPI0_PCS4_PORT]->PCR[SPI0_PCS4_PIN] = 0x0; //Clear
             PORT_PTRS[SPI0_PCS4_PORT]->PCR[SPI0_PCS4_PIN] |= PORT_PCR_MUX(SPI0_PCS4_ALT);
         }
-        if (chip_selects > 5 && spi_port == SPI0)
+        if (config.chip_selects > 5 && spi_port == SPI0)
         {
             PORT_PTRS[SPI0_PCS5_PORT]->PCR[SPI0_PCS5_PIN] = 0x0; //Clear
             PORT_PTRS[SPI0_PCS5_PORT]->PCR[SPI0_PCS5_PIN] |= PORT_PCR_MUX(SPI0_PCS5_ALT);
@@ -256,28 +256,51 @@ void SPI_init(uint8_t spi_port, SPI_mode_t mode, uint8_t chip_selects, bool PCSI
     // SPI Registers Config
     // MCR Config
     SPI_PTRS[spi_port]->MCR = 0x0 | SPI_MCR_HALT(1); //Clear and Halt - SPI is enabled at the end of the Init
-    SPI_PTRS[spi_port]->MCR |= SPI_MCR_MSTR(mode); //Define SPI mode Slave or Master
-    SPI_PTRS[spi_port]->MCR |= SPI_MCR_PCSIS(PCSIS); //Define SPI chip select inactive status as Active High or Active Low
+    SPI_PTRS[spi_port]->MCR |= SPI_MCR_MSTR(config.mode); //Define SPI mode Slave or Master
+    SPI_PTRS[spi_port]->MCR |= SPI_MCR_PCSIS(config.PCSIS); //Define SPI chip select inactive status as Active High or Active Low
     //SPI_PTRS[spi_port]->MCR |= SPI_MCR_CONT_SCKE(1);  //Turn SCLK Continuous mode on
     SPI_PTRS[spi_port]->MCR |= SPI_MCR_ROOE(1);  //RX FIFO overflow data into shift register 
 
     // CTAR Config
-    if(mode)
+    if(config.mode)
     {
         // CTAR Master Config
+        SPI_PTRS[spi_port]->CTAR = 0x0;
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_DBR(0); // 50/50 Clock duty cycle 
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_FMSZ(config.frame_size); // User defined frame size
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_LSBFE(config.LSB_first); // User defined LSB first in transfer or not
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_CPHA(config.clock_phase); // User defined SCK phase
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_CPOL(config.clock_polarity); // User defined SCK polarity
 
     }
     else
     {
         // CTAR Slave Config
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_SLAVE_FMSZ(config.frame_size); // User defined frame size
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_SLAVE_LSBFE(config.LSB_first); // User defined LSB first in transfer or not
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_SLAVE_CPHA(config.clock_phase); // User defined SCK phase
+        SPI_PTRS[spi_port]->CTAR |= SPI_CTAR_SLAVE_CPOL(config.clock_polarity); // User defined SCK polarity
 
     }
-
+    //Rest flags
+    SPI_PTRS[spi_port]->SR = 0x0;
+    SPI_PTRS[spi_port]->SR |= SPI_SR_TCF(1);
+    //Set Transfer Counter to 0
+    SPI_PTRS[spi_port]->TCR |= SPI_TCR_SPI_TCNT(0);
     //Enable SPI port
     SPI_PTRS[spi_port]->MCR |= SPI_MCR_HALT(0);
 
 }
 
+uint16_t SPI_Read(uint8_t spi_port)
+{
+    return SPI_PTRS[spi_port]->POPR;
+}
+
+void SPI_Write(uint8_t spi_port, uint16_t spi_data)
+{
+    SPI_PTRS[spi_port]->PUSHR = spi_data;
+}
 
 /*******************************************************************************
  *******************************************************************************
--- TPS/TP3/drivers/PDRV_SPI.h
+++ TPS/TP3/drivers/PDRV_SPI.h
@@ -27,6 +27,28 @@ enum {SPI0, SPI1, SPI2};
 
 typedef enum {SLAVE, MASTER} SPI_mode_t;
 
+/**
+ * @brief SPI_config_t - SPI configuration parameters data type
+ * @param mode - SPI port mode, either SLAVE or MASTER (check SPI_mode_t)
+ * @param chip_selects - number of peripheral chip selects used (SPI0: 1-6, SPI1: 1-4, SPI2: 1)
+ * @param frame_size - number of bits in a frame from 4 to 16
+ * @param clock_polarity - SCK Active High or Active low (e.g. clock_polarity = HIGH, then SCK is Active Low)
+ * @param clock_phase - Defines when data is captured and changed (e.g clock_phase = 0 - data is captured in leading edge and changed in the following edge)
+ * @param LSB_first - Defines if data is transfered as LSB first or MSB first (e.g LSB_first = TRUE, then data is transfered LSB first)
+ * @param PCSIS (only used in MASTER mode) - Peripheral Chip Select Inactive State - defines Chip select as either Active High or Active Low (e.g. PCSIS = HIGH, then chip select is Active Low)
+*/
+
+typedef struct SPI_config_t
+{
+	SPI_mode_t mode,
+	uint8_t chip_selects,
+	uint8_t frame_size, 
+	bool clock_polarity;
+	bool clock_phase;
+	bool LSB_first;
+	bool PCSIS
+}SPI_config_t;
+
 /*******************************************************************************
  * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
@@ -40,11 +62,15 @@ typedef enum {SLAVE, MASTER} SPI_mode_t;
 /**
  * @brief SPI_init: Initialize an SPI port
  * @param spi_port - SPI port to initialize (SPI0, SPI1, SPI2)
- * @param mode - SPI port mode - SLAVE or MASTER
- * @param chip_selects - Number of peripheral chip selects used (SPI0: 1-6, SPI1: 1-4, SPI2: 1) - Only relevant in MASTER mode
- * @param PCSIS - SPI Port Peripheral Chip Select Inactive State - Defines inactive state as High or Low (e.g. PCSIS = HIGH => Chip Select works as Active Low)
+ * @param config - User defined configuration of the SPI port (see SPI_config_t data type)
 */
-void SPI_init(uint8_t spi_port, SPI_mode_t mode, uint8_t chip_selects, bool PCSIS);
+void SPI_Init(uint8_t spi_port, SPI_config_t config);
+
+uint16_t SPI_Read(uint8_t spi_port);
+
+void SPI_Write(uint8_t spi_port, uint16_t spi_data);
+
+bool SPI_TransferComplete(uint8_t spi_port);
 
 /*******************************************************************************
  ******************************************************************************/
