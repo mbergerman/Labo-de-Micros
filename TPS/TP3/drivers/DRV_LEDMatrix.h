4865c8fa1e317ebd00e4b8e7846faf2b6d182f20
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2021-10-24 23:17:56 -0300

    Falto el .h

--- TPS/TP3/drivers/DRV_LEDMatrix.h
+++ TPS/TP3/drivers/DRV_LEDMatrix.h
@@ -41,6 +41,7 @@ typedef color_t LEDMatrix_t[HEIGHT][WIDTH];
 
 void LEDMatrix_init();
 void LEDMatrix_updateLED(color_t led, uint8_t height, uint8_t width);
+void LEDMatrix_updateBrightness(uint8_t brightness);
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
